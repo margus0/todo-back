822bc9fe8729a9dae7afbf650b250cbc6863dd97
margus0 <105805741+margus0@users.noreply.github.com>
2022-06-15 16:34:54 +0300

    Update validation.js

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 async function validateTodo(req, res, next) {
   const schema = Joi.object({
-    title: Joi.string().min(5).max(50).required(),
+    title: Joi.string().min(1).max(50).required(),
   });
 
   try {
